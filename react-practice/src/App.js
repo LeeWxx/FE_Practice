061cbfff2e816e5489270655cd34a573ef458354
leewxx <uchang903@naver.com>
2023-08-12 21:52:07 +0000

    ref

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-//import EventPractice from './EventPractice_Class';
-import EventPractice from './EventPractice_Function';
+import React, { Component } from 'react';
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return (
-		<EventPractice/>
-	);
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.ScrollBox=ref} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
