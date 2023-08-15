ef7050b0c4e6cf0383cc289c878a078a7dee00d8
leewxx <uchang903@naver.com>
2023-08-15 15:11:21 +0000

    컴포넌트 라이프 사이클

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import IterationSample from "./IterationSample";
+import LifeCycleSample from './Chapter7/LifeCycleSample';
+import ErrorBoundary from './Chapter7/ErrorBoundary';
 
+function getRandomColor() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 class App extends Component {
-  render() {
-    return (
-      <IterationSample />
-    );
-  }
+	state = {
+		color: '#000000',
+	};
+	handleClick = () => {
+		this.setState({
+			color: getRandomColor(),
+		});
+	};
+	render() {
+		return (
+			<div>
+				<button onClick={this.handleClick}>랜덤 색상</button>
+				<ErrorBoundary>
+					<LifeCycleSample color={this.state.color} />
+				</ErrorBoundary>
+			</div>
+		);
+	}
 }
 
 export default App;
