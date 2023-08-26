f57858a19858c99b7ece94664cdf8d34d3a6b65c
Leewxx <uchang903@naver.com>
2023-08-26 14:15:57 +0000

    immer를 사용하여 불변성 유지

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,63 +1,9 @@
 import React, { useState, useReducer, useRef, useCallback } from 'react';
-import TodoTemplate from './Chapter_10/TodoTemplate';
-import TodoInsert from './Chapter_10/TodoInsert';
-import TodoList from './Chapter_10/TodoList';
-
-function createBulkTodos() {
-	const array = [];
-	for (let i = 1; i <= 2500; i++) {
-		array.push({
-			id: i,
-			text: `할 일 ${i}`,
-			checked: false,
-		});
-	}
-	return array;
-}
-
-function todoReducer(todos, action) {
-	switch (action.type) {
-		case 'INSERT': // 새로 추가
-			return todos.concat(action.todo);
-		case 'REMOVE': // 제거
-			return todos.filter((todo) => todo.id !== action.id);
-		case 'TOGGLE': // 토글
-			return todos.map((todo) =>
-				todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
-			);
-		default:
-			return todos;
-	}
-}
+import ImmerComponent from './Chapter_12/ImmerComponent';
 
 const App = () => {
-	const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
-
-	const nextId = useRef(2501);
-
-	const onInsert = useCallback((text) => {
-		const todo = {
-			id: nextId.current,
-			text,
-			checked: false,
-		};
-		dispatch({ type: 'INSERT', todo });
-		nextId.current += 1;
-	}, []);
-
-	const onRemove = useCallback((id) => {
-		dispatch({ type: 'REMOVE', id });
-	}, []);
-
-	const onToggle = useCallback((id) => {
-		dispatch({ type: 'TOGGLE', id });
-	}, []);
-
 	return (
-		<TodoTemplate>
-			<TodoInsert onInsert={onInsert} />
-			<TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-		</TodoTemplate>
+		<ImmerComponent/>
 	);
 };
 
